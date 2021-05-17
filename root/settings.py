ccdf46a341c8ec07ef5af1b6abb283dd0ff0b945
poudyalanil <anilrunjakote@gmail.com>
2021-05-17 10:06:42 +0545

    Minute App Initialization

--- root/settings.py
+++ root/settings.py
@@ -50,6 +50,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #custom apps
+    'minute',
 
     #externals
     'rest_framework',
