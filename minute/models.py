ce2787dcd6168da4b9ef208aeca9008b439d23bc
poudyalanil <anilrunjakote@gmail.com>
2021-05-18 12:11:38 +0545

    Minute and agenda models

--- minute/models.py
+++ minute/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Agenda(models.Model):
+    agenda = models.CharField(max_length=255,blank=False,default=None,null=False)
+
+class Minute(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    minute = models.TextField(blank=False,null=False, default=None)
+    agenda = models.ManyToManyField(Agenda)
+    attendes = models.CharField(max_length=255) #dummy TBD
+    action_by = models.CharField(max_length=255,null=False,default=None,blank=False) #dummy TBD
+
+
+
