16dcf959cb02507cafd454859a7ed0c964ebeaba
Sameer Khan <contact@mdsameer.com.np>
2021-05-18 20:28:01 +0545

    feat: user and club models

---
+++ club/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+
+class HeadClub(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class BranchClub(models.Model):
+    # general details
+    active = models.BooleanField(default=True)
+    name = models.CharField(max_length=255)
+    head = models.ForeignKey(HeadClub, on_delete=models.CASCADE)
+    parent_club = models.CharField(
+        max_length=255, blank=False, default=None)
+    club_id = models.CharField(
+        max_length=255, blank=True, default=None, null=True)
+    motto = models.CharField(max_length=255, null=False,
+                             blank=False, default=None)
+    chartered_date = models.DateField()
+
+    # contact info
+    email = models.EmailField(blank=False, default=None)
+
+    # social media
+    website = models.URLField(
+        max_length=2048, default='https://asparksys.com', blank=True)
+    instagram = models.URLField(
+        max_length=2048, default='hittps://instagram.com/asparksystems', blank=True)
+    facebook = models.URLField(
+        max_length=2048, default='https://facebook.com/aspaksys', blank=True)
+    twitter = models.URLField(
+        max_length=2048, default='https://twitter.com/asparksys', blank=True)
+
+    # appearance
+    color_code = models.CharField(max_length=50, default='black', blank=False)
+    logo = models.URLField(
+        max_length=2048, default="https://4m4you.com/wp-content/uploads/2020/06/logo-placeholder.png")
+
+    def __str__(self):
+        return self.name
--- ums/models.py
+++ ums/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from club.models import BranchClub
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password):
+        if not email:
+            raise ValueError("invalid email")
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    objects = UserManager()
+    # fields
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    phone = models.CharField(max_length=15, null=True)
+    address = models.CharField(max_length=255, null=True)
+    club_id = models.ForeignKey(
+        BranchClub, on_delete=models.CASCADE, default=3)
+    admin = models.BooleanField(default=False)  # access django admin site
+    active = models.BooleanField(default=True)
+    user_image = models.URLField(
+        max_length=2048, default="https://picsum.photos/200")
+    # configs
+    USERNAME_FIELD = 'email'  # overwrite the built-in username field w/ email
+    # email & password are required by default, revisit to add extra required fields
+    REQUIRED_FIELDS = []
+
+    # methods
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.admin
+
+    def has_module_perms(self, perm, obj=None):
+        return self.admin
+
+    @property
+    def is_admin(self):
+        return self.admin
+
+    @property
+    def is_active(self):
+        return self.active
+
+    @property
+    def is_staff(self):
+        return self.admin
+
+    @property
+    def location(self):
+        return self.address
