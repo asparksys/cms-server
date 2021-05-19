31457ba474f80d1ff231c1c0b69ce68ec6c3e359
poudyalanil <anilrunjakote@gmail.com>
2021-05-19 18:10:23 +0545

    Minute Models

--- minute/models.py
+++ minute/models.py
@@ -1,14 +1,25 @@
 from django.db import models
+from ums.models import User
+from club.models import  BranchClub
 
-class Agenda(models.Model):
+class BaseModel():
+    club = models.ForeignKey(BranchClub, on_delete=models.CASCADE)
+    action_by = models.ForeignKey(User, on_delete=models.CASCADE) 
+
+class Agenda(BaseModel,models.Model):
     agenda = models.CharField(max_length=255,blank=False,default=None,null=False)
 
-class Minute(models.Model):
+    def __str__(self):
+        return self.agenda
+
+class Minute(BaseModel,models.Model):
     date = models.DateTimeField(auto_now_add=True)
     minute = models.TextField(blank=False,null=False, default=None)
     agenda = models.ManyToManyField(Agenda)
-    attendes = models.CharField(max_length=255) #dummy TBD
-    action_by = models.CharField(max_length=255,null=False,default=None,blank=False) #dummy TBD
+    attendes = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.date
 
 
 
