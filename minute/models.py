44d7a8e732af67985c551727fdfa9272d66522d2
Anil Poudyal <anilrunjakote@gmail.com>
2021-05-19 20:06:22 +0545

    Minute (#7)
    * configuration
    
    * Environment Configuration
    
    * Environment Setup
    
    * Minute App Initialization
    
    * Minute and agenda models
    
    * Minute Models
    
    * Minute Models
    
    * Minute Models Managed
    
    * Serializers and Minute API

--- minute/models.py
+++ minute/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from ums.models import User
+from club.models import  BranchClub
+from member.models import Member
 
-# Create your models here.
+class BaseModel(models.Model):
+    club = models.ForeignKey(BranchClub, blank=True,null=True, on_delete=models.CASCADE)
+    action_by = models.ForeignKey(User,blank=True,null=True,  on_delete=models.CASCADE) 
+    date_created = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
+
+class Minute(BaseModel):
+    minute = models.TextField(blank=True,null=True, default=None)
+    agenda = models.TextField(default=None)
+    attendes = models.ManyToManyField(Member)
+
+    def __str__(self):
+        return self.date
+
+
+
--- root/settings.py
+++ root/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "api",
     "club",
     "member",
+    'minute',
     "ums",
 ]
 
