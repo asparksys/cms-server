a30971783d4d79ae2d3ae87417fb56ab119cc0af
poudyalanil <anilrunjakote@gmail.com>
2021-05-19 19:57:33 +0545

    Serializers and Minute API

--- api/urls/base.py
+++ api/urls/base.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.conf.urls import url
 from .member import urlpatterns as member_urls
+from .minute import urlpatterns as minute_urls
 
 
-urlpatterns = [] + member_urls
+urlpatterns = [] + member_urls+minute_urls
--- minute/models.py
+++ minute/models.py
@@ -6,11 +6,11 @@ from member.models import Member
 class BaseModel(models.Model):
     club = models.ForeignKey(BranchClub, blank=True,null=True, on_delete=models.CASCADE)
     action_by = models.ForeignKey(User,blank=True,null=True,  on_delete=models.CASCADE) 
+    date_created = models.DateTimeField(auto_now=True)
     class Meta:
         abstract = True
 
 class Minute(BaseModel):
-    date = models.DateTimeField(auto_now_add=True)
     minute = models.TextField(blank=True,null=True, default=None)
     agenda = models.TextField(default=None)
     attendes = models.ManyToManyField(Member)
